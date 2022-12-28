be30413b3d169154e49cee92debe877298decb6f
Thomas <tberrios9800@gmail.com>
2022-12-27 23:08:59 -0500

    Added PyNaCl import

--- TBOT516 V3/botDependencies.py
+++ TBOT516 V3/botDependencies.py
@@ -4,7 +4,7 @@ import discord
 import random
 import yt_dlp
 import asyncio
+import nacl
 from discord.ext import commands
 from discord.utils import get
 from youtube_search import YoutubeSearch
-#PyNaCl is needed for connecting to voice channels
